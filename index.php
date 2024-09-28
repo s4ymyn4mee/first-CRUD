37e3543b6dee3e1156b5d21bf6c5c5d277b1b081
Vadim <vadim.ganiev25@mail.ru>
2024-09-29 03:04:50 +0500

    Added the ability to edit and delete records, added better numeration

--- index.php
+++ index.php
@@ -17,46 +17,40 @@
             <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Обратная связь</a></li>
         </ul>
         <ul class="nav">
-            <li class="nav-item"><a href="#" class="btn btn-outline-primary me-2">Войти</a></li>
+            <li class="nav-item"><a href="#" class="btn btn-outline-secondary me-2">Войти</a></li>
         </ul>
     </div>
 </nav>
 <div class="container mt-5">
     <h3>Список студентов:</h3>
-    <h6 class="m-3">
-        <form action="#" method="POST">
-            <div class="row mb-4">
-                <div class="col-2">
-                    <input type="text" name="name" value="" class="form-control" placeholder="Имя">
-                </div>
-                <div class="col-2">
-                    <input type="text" name="surname" class="form-control" placeholder="Фамилия">
-                </div>
-                <div class="col-2">
-                    <input type="text" name="age" class="form-control" placeholder="Возраст">
-                </div>
-                <div class="col">
-                    <input type="submit" name="add_student" class="btn btn-primary me-2" value="Добавить студента">
-                </div>
-            </div>
-
-
-        </form>
+    <h5 class="m-3">
         <?php
+        $name = $surname = $age = '';
         $connection_data = file("connection_data.txt",
-            FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) ?: [];
+            FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) ?? [];
         [$servername, $db_login, $db_password, $db_name] = $connection_data;
-        $connection = new mysqli(
-            $servername,
-            $db_login,
-            $db_password,
-            $db_name);
+        $connection = new mysqli($servername, $db_login, $db_password, $db_name);
+
+        $sql_query = "SELECT * FROM `students`";
+        $all_students = $connection->query($sql_query)->fetch_all(MYSQLI_ASSOC);
+        $connection->close();
+
+        if (count($all_students) > 0) {
+            $last_id = count($all_students) - 1;
+            $next_id = $all_students[$last_id]['id'] + 1;
+        } else {
+            $next_id = 1;
+        }
+
 
         if (isset($_POST['add_student'])) {
-            $name = $_POST['name'] ?: '';
-            $surname = $_POST['surname'] ?: '';
-            $age = $_POST['age'] ?: 0;
-            $sql_query = "INSERT INTO `students`(`name`,`surname`,`age`) VALUES ('$name','$surname','$age')";
+            $name = $_POST['name'] ?? '';
+            $surname = $_POST['surname'] ?? '';
+            $age = $_POST['age'] ?? '';
+
+            $connection = new mysqli($servername, $db_login, $db_password, $db_name);
+            $sql_query = "INSERT INTO `students`(`id`,`name`,`surname`,`age`) 
+                          VALUES ('$next_id','$name','$surname','$age')";
             $connection->query($sql_query);
             $connection->close();
 
@@ -64,27 +58,87 @@
             exit;
         }
 
+        if (isset($_GET['edit'])) {
+            $record_number = $_GET['edit'];
+            $name = $all_students[$record_number]['name'] ?? '';
+            $surname = $all_students[$record_number]['surname'] ?? '';
+            $age = $all_students[$record_number]['age'] ?? '';
+            $id = $all_students[$record_number]['id'] ?? '';
 
-        $connection = new mysqli(
-            $servername,
-            $db_login,
-            $db_password,
-            $db_name);
-        $sql_query = "SELECT * FROM `students`";
-        $query_result = $connection->query($sql_query)->fetch_all(MYSQLI_ASSOC);
+        }
 
-        foreach ($query_result as $key => $record) {
-            print_r("<p>
+        if (isset($_POST['save_student'])) {
+            if ($name != $_POST['name'] or $surname != $_POST['surname'] or $age != $_POST['age']) {
+                $name = $_POST['name'] ?? '';
+                $surname = $_POST['surname'] ?? '';
+                $age = $_POST['age'] ?? 0;
+
+                $connection = new mysqli($servername, $db_login, $db_password, $db_name);
+                $sql_query = "UPDATE `students`
+                          SET `name` = '$name',`surname` = '$surname',`age` = '$age' 
+                          WHERE `id` = '$id'";
+                $connection->query($sql_query);
+                $connection->close();
+            }
+
+            header("Location:" . $_SERVER['PHP_SELF']);
+            exit;
+        }
+
+        if (isset($_POST['delete_student'])) {
+            $connection = new mysqli($servername, $db_login, $db_password, $db_name);
+            $sql_query = "DELETE FROM `students` WHERE `id` = '$id'";
+            $connection->query($sql_query);
+            $connection->close();
+
+            header("Location:" . $_SERVER['PHP_SELF']);
+            exit;
+        }
+
+        ?>
+        <form action="#" method="POST">
+            <div class="row mb-4">
+                <div class="col-2">
+                    <input type="text" name="name" value="<?= $name ?>" class="form-control" placeholder="Имя">
+                </div>
+                <div class="col-2">
+                    <input type="text" name="surname" value="<?= $surname ?>" class="form-control"
+                           placeholder="Фамилия">
+                </div>
+                <div class="col-2">
+                    <input type="number" name="age" value="<?= $age ?>" class="form-control" placeholder="Возраст">
+                </div>
+                <?php if (!isset($_GET['edit'])): ?>
+                    <div class="col-auto">
+                        <button name="add_student" class="btn btn-outline-primary">Добавить студента</button>
+                    </div>
+                <?php endif; ?>
+                <?php if (isset($_GET['edit'])): ?>
+                    <div class="col-auto d-grid gap-1 d-md-block">
+                        <button name="save_student" class="btn btn-outline-primary">Сохранить</button>
+                        <button name="delete_student" class="btn btn-outline-danger">Удалить</button>
+                    </div>
+                <?php endif; ?>
+            </div>
+
+        </form>
+
+        <?php
+        foreach ($all_students as $key => $record) {
+            print_r("<p style='display: inline;'>
                     {$record['id']} |
                     {$record['name']} |
                     {$record['surname']} | 
-                    {$record['age']} </p>");
+                    {$record['age']}&emsp;
+                    <form method='GET' style='display: inline;'>
+                    <input type='hidden' name='edit' value='$key'>
+                    <input type='submit' class='btn btn-primary' value='Изменить'>
+                    </form>
+                    </p>");
         }
 
-        $connection->close();
-
         ?>
-    </h6>
+    </h5>
     <h3 class="mb-3 mt-4">До зимней сессии осталось:
         <?php
         const offset = 5;
